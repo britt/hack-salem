ea0bfc4403a642db7efa01af9351a9d21a6ff9fd
Britt Crawford <britt.v.crawford@gmail.com>
2017-11-05 22:06:23 -0800

    basic poll function

--- poll/functions/hello/index.js
+++ poll/functions/hello/index.js
@@ -1,5 +1,31 @@
-console.log('starting function')
-exports.handle = function(e, ctx, cb) {
-  console.log('processing event: %j', e)
-  cb(null, { hello: 'world' })
+const doc = require('dynamodb-doc')
+const dynamo = new doc.DynamoDB()
+
+exports.handle = function (e, ctx, cb) {
+  dynamo.putItem({
+    'TableName': 'votes',
+    'Item': {
+      'ip_addr': e.ipAddress,
+      'vote': e.vote
+    }
+  }, function (err, data) {
+    if (err) {
+      console.log(`error recording vote: ${err}`)
+      cb(null, { error: `error recording vote: ${err}` })
+    } else {
+      dynamo.scan({
+        'TableName': 'votes'
+      }, function (err, data) {
+        if (err) {
+          console.log(`error retrieving votes: ${err}`)
+          cb(null, { error: `error retrieving votes: ${err}` })
+        } else {
+          let votes = {}
+          console.log(data)
+          data.Items.forEach(d => { votes[d.vote] ? votes[d.vote] = votes[d.vote] + 1 : votes[d.vote] = 1 })
+          cb(null, { results: votes })
+        }
+      })
+    }
+  })
 }
